9c3b35d470c53ee2093b4a81410035f1a867b730
sachK01 <sac01m3@gmail.com>
2024-06-28 09:08:17 +0000

    ccc

--- js/first.js
+++ js/first.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+
+a = [0,1,2,3,4,5]
+i = 0
+j = a.length -1
+
+console.log("BEFORE : " + a)
+
+while(i < j){
+    temp = a[i]
+    a[i] = a[j]
+    a[j] = temp
+    i++
+    j--
+}
+
+console.log("AFTER  : " + a)
---
+++ js/string.js
@@ -0,0 +1,13 @@
+
+str = "babababbabab"
+i=0
+console.log("BEFORE : " + str)
+
+while(i < str.length){
+    if(str[i] === 'a'){
+        str.c
+    }
+    i++
+}
+
+console.log("AFTER : " + str)
