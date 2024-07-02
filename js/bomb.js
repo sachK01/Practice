59f010d7397b54c4aeb6477e2aafdec8aff60699
sachK01 <sac01m3@gmail.com>
2024-07-02 13:03:15 +0000

    sa

--- js/bomb.js
+++ js/bomb.js
@@ -17,20 +17,112 @@
 // let list = [["o", "a", "c"], ["d", "f", "g"],["w","x","z"]]
 // console.log(list[0])
 
-let list = []
+// let list = []
 
-const unique = iterable =>{
+// const unique = iterable =>{
     
-    console.log(iterable)
+//     console.log(iterable)
 
-    for(let i in iterable){
-        if(!list.includes(iterable[i]))
-            list.push(iterable[i])
+//     for(let i in iterable){
+//         if(!list.includes(iterable[i]))
+//             list.push(iterable[i])
+//     }
+
+//     return list
+// }
+
+// let l = unique("aaaabbbcceeeeaba")
+
+// console.log(l)
+
+// -------------2/7/2024-----------------
+
+// let a = [3,1,7,4,9,6]
+// console.log(Math.max(...a) - Math.min(...a));
+
+//Remove Duplicates from array
+// let arr = [11,55,22,11,33,22,11]
+// let arr1 = []
+
+// for(let i in arr){
+    
+//     if(!arr1.includes(arr[i]))
+//         arr1.push(arr[i])
+//     else{
+//         arr.splice(i,0)
+//     }
+// }
+
+// arr = arr1
+// console.log(arr1);
+
+//Count Occurences of Character in String
+
+/*let str = "ABCBACCCBCBCBC"
+let result = {}
+
+for(let i in str){
+    let ch = str.charAt(i)
+    
+    if(!result[ch]){
+        result[ch] = 1
     }
-
-    return list
+    else{
+        result[ch] += 1
+    }
 }
 
-let l = unique("aaaabbbcceeeeaba")
+console.log(result);*/
 
-console.log(l)
+//reverse the given string
+
+// let str = "Hello! I am from India!"
+// let arr = str.split(" ")
+
+// str = ""
+
+// for(let i in arr){
+//     str = arr[i] + " " + str
+// }
+
+// console.log(str);
+
+
+/*let result = 0
+const ab = (num) => {
+    
+    while(num != 0){
+        let rem = num % 10
+        num = Number.parseInt(num/10)
+
+        result = result * 10 + rem
+    }
+
+    return result
+};
+
+
+let ans = ab(1987)
+console.log(ans)*/
+
+//Callback
+
+
+function multiply(arr, callback){
+
+    let sum = 1
+
+    for(let i in arr){
+        sum *= arr[i]
+    }
+
+    callback()
+
+    return sum
+}
+
+let a = multiply([1,2,3,4,5], ()=>{
+    console.log("Hello Boys!");
+})
+
+console.log(a);
